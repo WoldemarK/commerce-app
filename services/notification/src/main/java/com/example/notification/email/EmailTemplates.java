eb4a438a8d26ae57aab940c95b39881b918cc9cf
WoldemarK <kovtynov.vladimir@gmail.com>
2024-11-02 23:04:20 +0300

    create new

--- services/notification/src/main/java/com/example/notification/email/EmailTemplates.java
+++ services/notification/src/main/java/com/example/notification/email/EmailTemplates.java
@@ -2,15 +2,14 @@ package com.example.notification.email;
 
 import lombok.Getter;
 
+@Getter
 public enum EmailTemplates {
 
     PAYMENT_CONFIRMATION("payment-confirmation.html", "Payment successfully processed"),
-    ORDER_CONFIRMATION("order-confirmation.html", "Order confirmation")
-            ;
+    ORDER_CONFIRMATION("order-confirmation.html", "Order confirmation");
 
-    @Getter
+
     private final String template;
-    @Getter
     private final String subject;
 
 
